0fb609b79675932433b9b017e3af4b81f2f5744e
CharlizeHo <hobaotranhn789@gmail.com>
2022-12-08 16:22:42 +0700

    Initial commit

---
+++ shop/src/App.jsx
@@ -0,0 +1,21 @@
+import Announcement from "./Components/Announcement";
+import Collection from "./Components/Collection";
+import Hero from "./Components/Hero";
+import Review from "./Components/Review";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+
+const App = () => {
+  return (
+    <div>
+      <Announcement />
+      <Hero />
+      <Collection />
+      <Review />
+      <About />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ shop/src/Components/Announcement.jsx
@@ -0,0 +1,15 @@
+const Announcement = () => {
+    return (
+      <section>
+        {/* Announcement */}
+        <div className="bg-[#4f46e5] text-white px-4 py-3 text-center text-sm font-medium">
+          <spam>Love Alpine JS? Check out this new course!</spam>{" "}
+          <a href="" className="underline">
+            Learn More →
+          </a>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Announcement;
